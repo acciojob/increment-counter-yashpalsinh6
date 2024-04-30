905acd3ffeec13ab52a722db75b387f3be215eb7
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-04-30 15:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const counterParagraph = document.getElementById("counter");
+const incrementButton = document.getElementById("incrementBtn");
+function  handleIncrement() {
+		let count = counterParagraph.textContent;
+		alert(count);
+		count++;
+		counterParagraph.textContent = count;
+	
+}
+
+incrementButton.addEventListener("click", handleIncrement);
